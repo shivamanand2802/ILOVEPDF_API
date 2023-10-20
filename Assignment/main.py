25b18afb73a041c54c408e71fab09f3014be2c92
Shivam Anand <136447409+shivamanand2802@users.noreply.github.com>
2023-10-20 21:06:38 +0530

    Update main.py

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,10 +1,15 @@
+
+# Import all required modules
 from pylovepdf.ilovepdf import ILovePdf
 
-
+# Class Based Functioning implementing OOPs concept of programming.
 class IlovePrdfAssignment:
+
+    # Constructor to assign the value of public key for further procedure. 
     def __init__(self) -> None:
         self.publickey = 'project_public_fafb8111ad9cdd7ebd42e905b6ba667a_W6ypIf200721c3e0405602e3e9b3b028207ea'
-
+        
+    # Method defined for merge operation. It takes pdfs as input and serve a single pdf as merged file.
     def combine_pdf(self, pdf_list: list) -> bool:
         ilovepdf = ILovePdf(self.publickey, verify_ssl=True)
         task = ilovepdf.new_task('merge')
@@ -18,6 +23,7 @@ class IlovePrdfAssignment:
             return True
         return False
 
+    # Method defined for split operation. It takes single pdf as input and make a seperation of pdf.
     def separate_pdf(self, file):
         ilovepdf = ILovePdf(self.publickey, verify_ssl=True)
         task = ilovepdf.new_task('split')
@@ -30,6 +36,7 @@ class IlovePrdfAssignment:
             return True
         return False
 
+    # Method defined for removal of password from a password secured pdf file. It takes encrypted pdf as input to decrypt it.
     def remove_password(self, file):
         ilovepdf = ILovePdf(self.publickey, verify_ssl=True)
         task = ilovepdf.new_task('unlock')
@@ -42,6 +49,7 @@ class IlovePrdfAssignment:
             return True
         return False
 
+    # Method defined for Text operation. It takes pdf as input and serve a single txt file with provided text within the pdf.
     def extract_text(self, file):
         ilovepdf = ILovePdf(self.publickey, verify_ssl=True)
         task = ilovepdf.new_task('extract')
@@ -54,6 +62,7 @@ class IlovePrdfAssignment:
             return True
         return False
 
+    # Method defined for conversion of image to pdf file. Inputed with image to get pdf output.
     def convert_jpg_to_pdf(self, img):
         ilovepdf = ILovePdf(self.publickey, verify_ssl=True)
         task = ilovepdf.new_task('imagetopdf')
@@ -70,6 +79,7 @@ class IlovePrdfAssignment:
         return False
 
 
+# Calling and Execution of above defined class.
 obj = IlovePrdfAssignment()
 obj.combine_pdf(["1.pdf", "2.pdf"])
 # obj.separate_pdf("1.pdf")
